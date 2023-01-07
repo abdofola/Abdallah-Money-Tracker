38e2095a0de7b2ed21181fdb6fe01429fadd09cc
abdofola <abdofola67@gmail.com>
2023-01-07 22:54:25 +0200

    commenting some unused code

--- next.config.js
+++ next.config.js
@@ -4,7 +4,18 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  ignoreBuildErrors: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
--- src/components/Chart/ChartWithTooltip.tsx
+++ src/components/Chart/ChartWithTooltip.tsx
@@ -1,111 +1,111 @@
 import { safeId } from "@lib/helpers";
-import React, { ComponentProps } from "react";
+import React from "react";
 import { PieChart } from "react-minimal-pie-chart";
 import ReactTooltip from "react-tooltip";
-import styles from "./Chart.module.css";
 
 
 const makeTooltipContent = (entry:any) =>
   `Sector ${entry.tooltip} has value ${entry.value}`;
 
 const ChartWithTooltip: React.FC<any> = ({ transactions, name }) => {
-  const [visible, setVisible] = React.useState({
-    income: true,
-    expense: true,
-  });
-  const [hover, setHover] = React.useState<null | number>(null);
-  const shiftSize = 0.5;
-  const cubicBezier = "cubic-bezier(.29, 1.01, 1, -0.68)";
-  const style = {
-    fontSize: "7px",
-    fontFamily: "sans-serif",
-  };
-  const normalized = transactions.reduce((acc:any, curr:any) => {
-    const { name, amount, color } = curr;
+  // const [visible, setVisible] = React.useState({
+  //   income: true,
+  //   expense: true,
+  // });
+  // const [hover, setHover] = React.useState<null | number>(null);
+  // const shiftSize = 0.5;
+  // const cubicBezier = "cubic-bezier(.29, 1.01, 1, -0.68)";
+  // const style = {
+  //   fontSize: "7px",
+  //   fontFamily: "sans-serif",
+  // };
+  // const normalized = transactions.reduce((acc:any, curr:any) => {
+  //   const { name, amount, color } = curr;
 
-    if (!(name in acc)) acc[name] = new Map();
+  //   if (!(name in acc)) acc[name] = new Map();
 
-    if (acc[name].has("amount"))
-      acc[name].set("amount", Number(amount) + acc[name].get("amount"));
-    else acc[name].set("amount", Number(amount));
+  //   if (acc[name].has("amount"))
+  //     acc[name].set("amount", Number(amount) + acc[name].get("amount"));
+  //   else acc[name].set("amount", Number(amount));
 
-    acc[name].set("color", color);
+  //   acc[name].set("color", color);
 
-    return acc;
-  }, {});
-  const data = Object.entries(normalized).map(([key, value], idx) => ({
-    key: safeId(key, idx),
-    title: key,
-    value: value.get("amount"),
-    color: value.get("color") ?? "gray",
-    tooltip: key,
-  }));
-  const text = visible[name] ? `hide ${name}` : `show ${name}`;
+  //   return acc;
+  // }, {});
+  // const data = Object.entries(normalized).map(([key, value], idx) => ({
+  //   key: safeId(key, idx),
+  //   title: key,
+  //   value: value.get("amount"),
+  //   color: value.get("color") ?? "gray",
+  //   tooltip: key,
+  // }));
+  // const text = visible[name] ? `hide ${name}` : `show ${name}`;
 
-  const toggleChartVisibility = (name: MyChart["name"]) => () =>
-    setVisible({ ...visible, [name]: !visible[name] });
+  // const toggleChartVisibility = (name: MyChart["name"]) => () =>
+  //   setVisible({ ...visible, [name]: !visible[name] });
 
-  return (
-    <div data-tip="" data-for="chart" className={styles.chart}>
-      <>
-        <button
-          className={styles.chart__title}
-          onClick={toggleChartVisibility(name)}
-        >
-          {text} chart
-        </button>
-        {visible[name] && (
-          <div className="sm:px-4">
-            <PieChart
-              animate
-              radius={PieChart.defaultProps.radius - shiftSize}
-              lineWidth={50}
-              labelPosition={75}
-              animationEasing={cubicBezier}
-              label={({ x, y, dx, dy, dataEntry }) => (
-                <text
-                  key={dataEntry.key}
-                  x={x}
-                  y={y}
-                  dx={dx}
-                  dy={dy}
-                  dominantBaseline="central"
-                  textAnchor="middle"
-                  style={style}
-                >
-                  {Math.round(dataEntry.percentage) + "%"}
-                </text>
-              )}
-              labelStyle={style}
-              segmentsShift={() => shiftSize}
-              data={data}
-              onMouseOver={(_, segmentIdx) => setHover(segmentIdx)}
-              onMouseOut={() => setHover(null)}
-            />
-            <ReactTooltip
-              id="chart"
-              getContent={() => {
-                return typeof hover === "number"
-                  ? makeTooltipContent(data[hover])
-                  : null;
-              }}
-            />
-            <ul className={styles.chart__indicators}>
-              {data.map(({ title, color }) => (
-                <li key={title} className={styles.chart__indicator}>
-                  <span
-                    className="inline-block w-3 h-3 self-end rounded-full"
-                    style={{ backgroundColor: `${color}` }}
-                  />
-                  <span>{title}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </>
-    </div>
-  );
+  // return (
+  //   <div data-tip="" data-for="chart" className={styles.chart}>
+  //     <>
+  //       <button
+  //         className={styles.chart__title}
+  //         onClick={toggleChartVisibility(name)}
+  //       >
+  //         {text} chart
+  //       </button>
+  //       {visible[name] && (
+  //         <div className="sm:px-4">
+  //           <PieChart
+  //             animate
+  //             radius={PieChart.defaultProps.radius - shiftSize}
+  //             lineWidth={50}
+  //             labelPosition={75}
+  //             animationEasing={cubicBezier}
+  //             label={({ x, y, dx, dy, dataEntry }) => (
+  //               <text
+  //                 key={dataEntry.key}
+  //                 x={x}
+  //                 y={y}
+  //                 dx={dx}
+  //                 dy={dy}
+  //                 dominantBaseline="central"
+  //                 textAnchor="middle"
+  //                 style={style}
+  //               >
+  //                 {Math.round(dataEntry.percentage) + "%"}
+  //               </text>
+  //             )}
+  //             labelStyle={style}
+  //             segmentsShift={() => shiftSize}
+  //             data={data}
+  //             onMouseOver={(_, segmentIdx) => setHover(segmentIdx)}
+  //             onMouseOut={() => setHover(null)}
+  //           />
+  //           <ReactTooltip
+  //             id="chart"
+  //             getContent={() => {
+  //               return typeof hover === "number"
+  //                 ? makeTooltipContent(data[hover])
+  //                 : null;
+  //             }}
+  //           />
+  //           <ul className={styles.chart__indicators}>
+  //             {data.map(({ title, color }) => (
+  //               <li key={title} className={styles.chart__indicator}>
+  //                 <span
+  //                   className="inline-block w-3 h-3 self-end rounded-full"
+  //                   style={{ backgroundColor: `${color}` }}
+  //                 />
+  //                 <span>{title}</span>
+  //               </li>
+  //             ))}
+  //           </ul>
+  //         </div>
+  //       )}
+  //     </>
+  //   </div>
+  // );
+  return <></>
 };
 
 export default ChartWithTooltip;
