1464775f24f71bd07b4be5d382bef674df880d8c
abdofola <abdofola67@gmail.com>
2023-01-30 22:23:56 +0200

    allow cors

--- src/lib/enviroment.ts
+++ src/lib/enviroment.ts
@@ -1,5 +1,5 @@
 export const enviroment = {
   development: process.env.NEXT_PUBLIC_DEV_API,
-  test: process.env.NEXT_PUBLIC_API,
+  test: process.env.NEXT_PUBLIC_DEV_API,
   production: process.env.NEXT_PUBLIC_PROD_API,
 };
